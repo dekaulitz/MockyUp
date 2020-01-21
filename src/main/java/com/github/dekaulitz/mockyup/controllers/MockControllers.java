a592f9dc199d29bc3b87604e38b747a996a719c9
SulaimanFahmi <fahmi.sulaiman@ovo.id>
2020-01-21 18:24:03 +0700

    add swagger file and update docs on controller

--- src/main/java/com/github/dekaulitz/mockyup/controllers/MockControllers.java
+++ src/main/java/com/github/dekaulitz/mockyup/controllers/MockControllers.java
@@ -16,6 +16,8 @@ import io.swagger.v3.oas.models.Paths;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,6 +25,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 @RestController
@@ -47,10 +52,34 @@ public class MockControllers {
         }
         return map;
     }
-    @GetMapping(value = "/")
-    public ResponseEntity greeting() {
-        return ResponseEntity.ok("mock example");
+
+    /**
+     * @desc showing swagger doc
+     * @return
+     * @throws IOException
+     */
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> greeting() throws IOException {
+        Resource resource = new ClassPathResource("/static/swagger.json");
+        InputStreamReader isReader = new InputStreamReader(resource.getInputStream());
+        BufferedReader reader = new BufferedReader(isReader);
+        StringBuffer sb = new StringBuffer();
+        String str;
+        while ((str = reader.readLine()) != null) {
+            sb.append(str);
+        }
+        return ResponseEntity.ok(sb.toString());
     }
+
+    /**
+     * @desc mocking the uri that registerion on mockyup
+     * @param path
+     * @param id
+     * @param body
+     * @param request
+     * @return
+     * @throws JsonProcessingException
+     */
     @RequestMapping(value = "/mocks/mocking/{id}", method = {RequestMethod.OPTIONS, RequestMethod.DELETE,
             RequestMethod.POST, RequestMethod.GET, RequestMethod.HEAD, RequestMethod.PATCH, RequestMethod.PUT,
             RequestMethod.TRACE},
@@ -73,6 +102,9 @@ public class MockControllers {
         mockResponseVmodel.setTitle(mockEntities.get().getTitle());
         OpenAPI openAPI = (OpenAPI) mockResponseVmodel.getSpec();
         final Object[] response = new Object[1];
+        /*
+        checking every path that registering on that id and matching with request path
+         */
         for (Map.Entry<String, PathItem> entry : openAPI.getPaths().entrySet()) {
             String s = entry.getKey();
             PathItem pathItem = entry.getValue();
@@ -80,7 +112,13 @@ public class MockControllers {
             String[] openAPIPaths = openApiPath.split("/");
             String[] paths = originalPathUri[0].split("/");
             String[] regexPath = new String[paths.length];
+            /*
+                every targeting mocks uri should has same length
+             */
             if (openAPIPaths.length == paths.length) {
+                /*
+                converting request path to original for searching data
+                 */
                 for (int i = 0; i < openAPIPaths.length; i++) {
                     if (!openAPIPaths[i].equals(paths[i])) {
                         if (openAPIPaths[i].contains("*")) {
@@ -90,11 +128,17 @@ public class MockControllers {
                         regexPath[i] = openAPIPaths[i];
                     }
                 }
+                /*
+                find every path that equeal with orginal request path
+                 */
                 if (openApiPath.equals(String.join("/", regexPath))) {
                     JsonNode jsonNode = Json.mapper().valueToTree(pathItem);
                     Operation ops = Json.mapper().treeToValue(jsonNode.get(request.getMethod().toLowerCase()), Operation.class);
                     Map<String, List<Map<String, Object>>> examples = (Map<String, List<Map<String, Object>>>) ops.getExtensions().get(MockModel.X_EXAMPLES);
                     for (Map.Entry<String, List<Map<String, Object>>> extension : examples.entrySet()) {
+                       /*
+                       if there some example on headers will check every field on request header and mapping with example
+                        */
                         if (extension.getKey().equals(MockModel.X_HEADERS)) {
                             for (Map<String, Object> stringObjectMap : extension.getValue()) {
                                 MockExample mockExample = Json.mapper().convertValue(stringObjectMap, MockExample.class);
@@ -103,6 +147,9 @@ public class MockControllers {
                                 }
                             }
                         }
+                        /*
+                         if there some example on body will check every field on request body and mapping with example
+                         */
                         if (extension.getKey().equals(MockModel.x_BODY)) {
                             if (request.getMethod().equals(HttpMethod.POST.toString()) || request.getMethod().equals(HttpMethod.PATCH.toString())
                                     || request.getMethod().equals(HttpMethod.PUT.toString()))
@@ -114,6 +161,9 @@ public class MockControllers {
                                     }
                                 }
                         }
+                         /*
+                         if there some example on path will check every field on request body and mapping with example
+                         */
                         if (extension.getKey().equals(MockModel.x_PATH)) {
                             for (Map<String, Object> stringObjectMap : extension.getValue()) {
                                 MockExample mockExample = Json.mapper().convertValue(stringObjectMap, MockExample.class);
@@ -128,6 +178,9 @@ public class MockControllers {
                                 }
                             }
                         }
+                            /*
+                         if there some on query on path will check every field on request body and mapping with example
+                         */
                         if (extension.getKey().equals(MockModel.x_QUERY)) {
                             for (Map<String, Object> stringObjectMap : extension.getValue()) {
                                 MockExample mockExample = Json.mapper().convertValue(stringObjectMap, MockExample.class);
@@ -154,6 +207,10 @@ public class MockControllers {
         return responseEntity;
     }
 
+    /**
+     * @desc listing all mocks
+     * @return
+     */
     @GetMapping(value = "/mocks")
     public ResponseEntity<List<MockVmodel>> listingMockRepositories() {
         List<MockVmodel> mockResponseVmodels = new ArrayList<>();
@@ -179,6 +236,12 @@ public class MockControllers {
         return ResponseEntity.ok(mockResponseVmodels);
     }
 
+    /**
+     * @desc storing the mocks
+     * @param body
+     * @return
+     * @throws JsonProcessingException
+     */
     @PostMapping(value = "/mocks")
     public ResponseEntity<MockVmodel> storeMocksEntity(@RequestBody MockVmodel body) throws JsonProcessingException {
         SwaggerParseResult result = new OpenAPIParser().readContents(Json.mapper().writeValueAsString(body.getSpec()), null, null);
