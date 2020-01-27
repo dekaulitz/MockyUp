891e877f52edd0c3c52dc6fe0d65beb283c8c02e
SulaimanFahmi <fahmi.sulaiman@ovo.id>
2020-01-27 12:10:43 +0700

    add shell script

--- runner.sh
+++ runner.sh
@@ -4,26 +4,26 @@ envType=$3
 
 #check environment
 if [ -z $env ]; then
-    ENVIRONMENT=.env.dev
+  ENVIRONMENT=.env.dev
 else
   if [ $env == "-e" ]; then
-      if [ -z $envType ]; then
-          ENVIRONMENT=.env.dev
-      else
-         ENVIRONMENT=.env.${envType}
-      fi
+    if [ -z $envType ]; then
+      ENVIRONMENT=.env.dev
+    else
+      ENVIRONMENT=.env.${envType}
+    fi
   fi
 fi
 
 . $ENVIRONMENT
 
+echo running $ENVIRONMENT
 #run by type
 if [ $type == "docker" ]; then
-    mvn install
-    docker build -t mockup:latest .
-    docker run -itd -e DATABASE_HOST=$DATABASE_HOST -e SERVER_PORT=$PORT -p 8080:$PORT mockup:latest
+  mvn install
+  docker build -t mockup:latest .
+  docker run -itd -e DATABASE_HOST=$DATABASE_HOST -e SERVER_PORT=$SERVER_PORT -p 8080:$SERVER_PORT mockup:latest
 elif [ $type == "run" ]; then
-   mvn install
-   java -jar ./target/mockyup-0.0.1-SNAPSHOT.jar
- fi
-
+  mvn install
+  java -jar ./target/mockyup-0.0.1-SNAPSHOT.jar
+fi
