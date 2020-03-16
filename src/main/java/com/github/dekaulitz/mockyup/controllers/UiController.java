cd3caf47d6bf99cbf781245dad251645868205b2
dekaulitz <sulaimanfahmi@gmail.com>
2020-03-16 23:24:02 +0700

    fixing html 5 mode on refresh

--- src/main/java/com/github/dekaulitz/mockyup/controllers/UiController.java
+++ src/main/java/com/github/dekaulitz/mockyup/controllers/UiController.java
@@ -3,6 +3,7 @@ package com.github.dekaulitz.mockyup.controllers;
 import com.github.dekaulitz.mockyup.configuration.logs.LogsMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UiController extends BaseController {
@@ -15,6 +16,10 @@ public class UiController extends BaseController {
     public String loadUi() {
         return "index";
     }
-
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirect() {
+        // Forward to home page so that route is preserved.
+        return "forward:/";
+    }
 
 }
