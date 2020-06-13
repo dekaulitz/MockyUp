94fc8edbd1adb16d0cd495006231cb803072d88c
dekaulitz <sulaimanfahmi@gmail.com>
2020-06-13 20:22:01 +0700

    mising git cmmit

---
+++ src/main/java/com/github/dekaulitz/mockyup/utils/ConstantsRepository.java
@@ -0,0 +1,8 @@
+package com.github.dekaulitz.mockyup.utils;
+
+public class ConstantsRepository {
+
+    public final static String REQUEST_ID = "requestId";
+    public final static String REQUEST_TIME = "responseTime";
+    public final static String PATH_ENDPOINT = "endPoint";
+}
