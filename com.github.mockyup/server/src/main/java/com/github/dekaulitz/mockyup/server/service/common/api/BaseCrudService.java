08ac7b8bc09d69ef43f9876413381b5eafa09c00
dekaulitz <sulaimanfahmi@gmail.com>
2021-10-25 23:39:43 +0700

    fixing issue

---
+++ com.github.mockyup/server/src/main/java/com/github/dekaulitz/mockyup/server/service/common/api/BaseCrudService.java
@@ -0,0 +1,27 @@
+package com.github.dekaulitz.mockyup.server.service.common.api;
+
+import com.github.dekaulitz.mockyup.server.db.entities.BaseMongoEntity;
+import com.github.dekaulitz.mockyup.server.errors.ServiceException;
+import java.util.List;
+import org.springframework.data.mongodb.core.query.Query;
+
+public interface BaseCrudService<T extends BaseMongoEntity> {
+
+  T getById(String id, Class<T> entityClass) throws ServiceException;
+
+  T findOne(Query query, Class<T> entityClass) throws ServiceException;
+
+  void delete(T t) throws ServiceException;
+
+  void deleteById(String id) throws ServiceException;
+
+  List<T> getAll(Query query, Class<T> entityClass);
+
+  T update(String id, T t) throws ServiceException;
+
+  T update(T t) throws ServiceException;
+
+  T save(T t) throws ServiceException;
+
+  Boolean isExists(Query query, Class<T> entityClass);
+}
